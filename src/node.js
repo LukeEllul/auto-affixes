382e7edceb9bfc8a48db3b95583c44cba62b4b4a
LukeEllul <ellul.luke@gmail.com>
2018-05-12 19:26:44 +0200

    added peoperties functionality and added getDeepPropertyFunction

--- src/node.js
+++ src/node.js
@@ -6,29 +6,34 @@ const { Either } = require('../x/X');
  */
 
 /**
- * newNode :: char -> bool -> Number -> {n: Node} -> Node -> b
+ * newNode :: char -> bool -> Number -> Object -> {n: Node} -> Node -> b
  */
-const newNode = R.curry((a, b, n, children, Node) => Node(a)(b)(n)(children));
+const newNode = R.curry((a, b, n, props, children, Node) => Node(a)(b)(n)(props)(children));
 
 /**
  * getNodeCharacter :: Node -> char
  */
-const getNodeCharacter = Node => Node(a => _ => _ => _ => a);
+const getNodeCharacter = Node => Node(a => _ => _ => _ => _ => a);
 
 /**
  * getNodeBool :: Node -> bool
  */
-const getNodeBool = Node => Node(_ => b => _ => _ => b);
+const getNodeBool = Node => Node(_ => b => _ => _ => _ => b);
 
 /**
  * getNodeCount :: Node -> Number
  */
-const getNodeCount = Node => Node(_ => _ => n => _ => n);
+const getNodeCount = Node => Node(_ => _ => n => _ => _ => n);
 
 /**
  * getNodeChildren :: Node -> {n: Node}
  */
-const getNodeChildren = Node => Node(_ => _ => _ => children => children);
+const getNodeChildren = Node => Node(_ => _ => _ => _ => children => children);
+
+/**
+ * getNodeProperties :: Node -> Object
+ */
+const getNodeProperties = Node => Node(_ => _ => _ => props => _ => props);
 
 /**
  * childExists :: char -> Node -> bool
@@ -44,16 +49,16 @@ const getNodeChild = R.curry((c, Node) => getNodeChildren(Node)[c])
  * addChild :: Node -> Node -> Node
  */
 const addChild = R.curry((parent, child) =>
-    parent(a => b => n => children => newNode(a, b, n, { ...children, [getNodeCharacter(child)]: child })));
+    parent(a => b => n => props => children => newNode(a, b, n, props, { ...children, [getNodeCharacter(child)]: child })));
 
 /**
  * removeChild :: char -> Node -> Node
  */
 const removeChild = R.curry((parent, char) =>
-    parent(a => b => n => children => newNode(a, b, n, R.omit([char], children))));
+    parent(a => b => n => props => children => newNode(a, b, n, props, R.omit([char], children))));
 
 /**
- * updateDeepNode :: (char -> bool -> Number -> {n: Node} -> Node) -> String -> Node -> Node
+ * updateDeepNode :: (char -> bool -> Number -> Object -> {n: Node} -> Node) -> String -> Node -> Node
  */
 const updateDeepNode = R.curry((f, s, Node) =>
     s.length === 1 ? Node(f) : childExists(R.head(R.tail(s)), Node) ?
@@ -61,16 +66,24 @@ const updateDeepNode = R.curry((f, s, Node) =>
             addChild(Node, updateDeepNode(f, R.tail(s), getNodeChild(R.head(R.tail(s)), Node))) :
         Node);
 
+/**
+ * getDeepProperty :: (char -> bool -> Number -> Object -> {n : Node} -> a) -> String -> Node -> a
+ */
+const getDeepProperty = R.curry((f, s, Node) =>
+    s.length === 1 ? Node(f) : childExists(R.head(R.tail(s)), Node) ? 
+        s.length === 2 ? getNodeChild(R.tail(s), Node)(f) :
+        getDeepProperty(f, R.tail(s), getNodeChild(R.head(R.tail(s)), Node)) : undefined);
+
 // const updateDeepNode = R.curry((f, s, Node) =>
 //     s.length === 1 ? Node(f) :
 //         childExists(R.head(R.tail(s)), Node) ?
 //             addChild(removeChild(Node, R.head(R.tail(s))), ))
 
-// const root = newNode('r', false, 20, {});
-// const c1 = newNode('e', false, 12, {});
-// const c2 = newNode('d', false, 13, {});
+// const root = newNode('r', false, 20, {}, {});
+// const c1 = newNode('e', false, 12, {}, {});
+// const c2 = newNode('d', false, 13, {}, {});
 
-// const newRoot = addChild(addChild(root, c1), c2);
+// const c3 = addChild(c2, newNode('q', false, 25, {}, {}));
 
 // const updatedRoot = R.pipe(
 //     updateDeepNode(_ => _ => _ => _ => newNode('f', false, 23, {}), 're'),
@@ -87,5 +100,6 @@ module.exports = {
     removeChild,
     childExists,
     getNodeChildren,
-    getNodeCharacter
-}
+    getNodeCharacter,
+    getDeepProperty
+};
--- src/trie.js
+++ src/trie.js
@@ -3,20 +3,21 @@ const {newNode, updateDeepNode, addChild, childExists, getNodeChildren, getNodeC
 const fs = require('fs');
 
 /**
- * createBranch :: char -> bool -> Number -> String -> Node -> Node
+ * createBranch :: char -> bool -> Number -> Object -> String -> Node -> Node
  */
-const createBranch = R.curry((a, b, n) =>
-    updateDeepNode(A => B => N => children => 
-        newNode(A, B, N, children[a] ? children : {...children, [a]: newNode(a, b, n, {})})));
+const createBranch = R.curry((a, b, n, props) =>
+    updateDeepNode(A => B => N => Props => children =>
+        newNode(A, B, N, Props, children[a] ? children : {...children, [a]: newNode(a, b, n, props, {})})));
 
 /**
  * store :: Node -> Object
  */
 const store = Node => 
-    Node(a => b => n => children => ({
+    Node(a => b => n => props => children => ({
         [a]: {
             gold: b,
             count: n,
+            props: props,
             ...R.map(node => store(node)[getNodeCharacter(node)])(children)
         }
     }));
@@ -25,8 +26,8 @@ const store = Node =>
  * restore :: Object -> Node
  */
 const restore = (o, n = 0) => R.pipe(
-    _ => R.mapObjIndexed((v, k) => newNode(k, v.gold, v.count, R.pipe(
-        _ => R.omit(['gold', 'count'])(v),
+    _ => R.mapObjIndexed((v, k) => newNode(k, v.gold, v.count, v.props, R.pipe(
+        _ => R.omit(['gold', 'count', 'props'])(v),
         o => restore(o, 1)
     )()))(o),
     o => n === 0 ? o[Object.keys(o)[0]] : o
@@ -38,32 +39,24 @@ const restore = (o, n = 0) => R.pipe(
 const insertWord = R.curry((word, b, n, Node) =>
     R.pipe(
         ...Array.from(R.tail(word)).map((c, i) => 
-            createBranch(c, i === word.length - 2 ? b : false, i === word.length - 2 ? n : 0)(R.take(i + 1, word)))
+            createBranch(c, i === word.length - 2 ? b : false, i === word.length - 2 ? n : 0, {})(R.take(i + 1, word)))
     )(Node));
 
 /**
  * containsWord :: String -> Node -> bool
  */
 const containsWord = R.curry((s, Node) =>
-    Node(a => b => _ => children => 
+    Node(a => b => _ => props => children => 
         a === s && b === true ? true : R.pipe(node => node ? containsWord(R.tail(s), node) : false)(children[R.head(R.tail(s))])));
 
 /**
  * rootNode :: Node
  */
-const rootNode = newNode(' ', false, 0, 
-    Object.assign({}, ...R.range(97, 123).map(s => String.fromCharCode(s)).map(c => ({[c]: newNode(c, false, 0, {})}))));
+const rootNode = newNode(' ', false, 0, {},
+    Object.assign({}, ...R.range(97, 123).map(s => String.fromCharCode(s)).map(c => ({[c]: newNode(c, false, 0, {}, {})}))));
 
 /**
  * insertWords :: {String: Number} -> Node -> Node
  */
-const insertWords = map => R.pipe(
-    ...R.pipe(o => Object.keys(o).map(k => o[k]))(R.mapObjIndexed((v, k) => insertWord(' ' + k, true, v))(map))
-);
-
-/**
- * insertWordList :: {String: Number} -> Node -> Node
- */
-const insertWordList = map => R.pipe(
-    ...R.splitEvery(3500, Object.keys(map)).map(keys => insertWords(Object.assign({}, ...keys.map(k => ({[k]: map[k]})))))
-);
+const insertWords = R.curry((map, node) =>
+    Object.keys(map).reduce((node, k) => insertWord(k, true, map[k], node), node));
